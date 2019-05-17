de4b82575e6df8d27eafdf3b4befba9a49ae7278
zccz14 <zccz14@outlook.com>
2019-05-18 02:41:20 +0800

    :sparkles: createActionFactory

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/**
+ * create action factory
+ *
+ * @param type the identifier of actions, defaults to a unique symbol
+ * @returns an action factory with type predicate
+ */
+export const createActionFactory = <T = void>(type: any = Symbol()) =>
+  Object.assign(
+    /**
+     * action factory (creator)
+     * @param payload the payload of the created action
+     */
+    (payload: T) => ({
+      /** the type of action, NEVER TO READ IT */
+      type,
+      /** the payload of the action */
+      payload,
+    }),
+    {
+      /**
+       * Is `a` an action created by this action factory?
+       * @param a anything treated as an action
+       */
+      match: (a: any): a is { payload: T } => a && a.type === type,
+    },
+  );
