77d966272c107771bf4e1b48ffde4352e7a03fb7
zccz14 <zccz14@outlook.com>
2019-05-19 01:44:32 +0800

    :recycle: Rename API

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,11 @@
  * @param type the identifier of actions, defaults to a unique symbol
  * @returns an action factory with type predicate
  */
-export const createActionFactory = <T = void>(type: any = Symbol()) =>
+export const ActionCreator = <T = void>(type: any = Symbol()) =>
   Object.assign(
     /**
      * action factory (creator)
+     * 
      * @param payload the payload of the created action
      */
     (payload: T) => ({
@@ -18,7 +19,8 @@ export const createActionFactory = <T = void>(type: any = Symbol()) =>
     }),
     {
       /**
-       * Is `a` an action created by this action factory?
+       * Returns true if `a` is an action created by this action factory.
+       * 
        * @param a anything treated as an action
        */
       match: (a: any): a is { payload: T } => a && a.type === type,
