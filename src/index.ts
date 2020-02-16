2686b7ed8f9cf6a842617ea8e45d64a58d0d6b85
zheng.chen <zccz14@outlook.com>
2020-02-16 13:35:37 +0800

    :sparkles: add new types

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,33 @@
+export interface IAction<T = void> {
+  /** unknown type */
+  type: unknown;
+  /** the payload of the action */
+  payload: T;
+}
+
+export interface IActionCreator<T = void> {
+  /**
+   * action creator
+   *
+   * @param payload the payload of the created action
+   */
+  (payload: T): IAction<T>;
+  /**
+   * Returns true if `a` is an action created by this action creator.
+   *
+   * @param a anything treated as an action
+   */
+  match: (a: any) => a is IAction<T>;
+}
+
 /**
- * new Action Creator
+ * create an Action Creator
  *
  * @param type the identifier of actions, defaults to a unique symbol
- * @returns an action factory with type predicate
+ * @template T the type of action payload
+ * @returns an action creator with type predicate
  */
 export const ActionCreator = <T = void>(type: any = Symbol()) =>
-  Object.assign(
-    /**
-     * action creator
-     * 
-     * @param payload the payload of the created action
-     */
-    (payload: T) => ({
-      /** the type of action, NEVER READ IT */
-      type,
-      /** the payload of the action */
-      payload,
-    }),
-    {
-      /**
-       * Returns true if `a` is an action created by this action creator.
-       * 
-       * @param a anything treated as an action
-       */
-      match: (a: any): a is { payload: T } => a && a.type === type,
-    },
-  );
+  Object.assign((payload: T) => ({ type, payload }), {
+    match: (a: any) => a && a.type === type,
+  }) as IActionCreator<T>;
